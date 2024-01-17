41eb6ae6267b7d0b87d0e3b3be9abc3adbd32ed4
mbetz <mbetzel1@gmail.com>
2024-01-16 21:19:12 -0500

    javascript for topnav slow transition

--- scripts/navbar_hide.js
+++ scripts/navbar_hide.js
@@ -1,12 +1,28 @@
-var lastScrollTop;
-navbar = document.getElementById('topnav-holder');
-window.addEventListener('scroll',function(){
-var scrollTop = window.scrollY || document.documentElement.scrollTop;
-if(scrollTop > lastScrollTop){
-navbar.style.top='-80px';
-}
-else{
-navbar.style.top='0';
-}
-lastScrollTop = scrollTop;
-});
+// var lastScrollTop;
+// navbar = document.getElementById('topnav-holder');
+// window.addEventListener('scroll',function(){
+// var scrollTop = window.scrollY || document.documentElement.scrollTop;
+// if(scrollTop > lastScrollTop){
+// navbar.style.top='-80px';
+// }
+// else{
+// navbar.style.top='0';
+// }
+// lastScrollTop = scrollTop;
+// });
+
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function() {
+  let currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("topnav-holder").classList.remove("hidden");
+  } else {
+    if (currentScrollPos > window.innerHeight / 2) {
+      document.getElementById("topnav-holder").classList.add("hidden");
+    }
+  }
+
+  prevScrollPos = currentScrollPos;
+};
