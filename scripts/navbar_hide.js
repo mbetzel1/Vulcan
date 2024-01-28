f1c75570df4a16976e34a96d40e2488afd1577e6
mbetz <mbetzel1@gmail.com>
2024-01-28 18:20:15 -0500

    topnav transition

--- scripts/navbar_hide.js
+++ scripts/navbar_hide.js
@@ -13,3 +13,16 @@ window.onscroll = function() {
 
   prevScrollPos = currentScrollPos;
 };
+
+window.onscroll = function() {
+  shrinkNavbar();
+};
+
+function shrinkNavbar() {
+  var navbar = document.getElementById("topnav-holder");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      navbar.style.padding = "0vh 10vw"; // Set the desired height when scrolled
+  } else {
+      navbar.style.padding = "3vh 10vw"; // Set the original height when at the top
+  }
+};
