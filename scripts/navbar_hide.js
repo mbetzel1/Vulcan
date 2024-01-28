2df98adf05bfda2c1d12fc0c648fe59d57fa186d
mbetz <mbetzel1@gmail.com>
2024-01-28 18:55:10 -0500

    better pictures

--- scripts/navbar_hide.js
+++ scripts/navbar_hide.js
@@ -14,15 +14,15 @@ window.onscroll = function() {
   prevScrollPos = currentScrollPos;
 };
 
-window.onscroll = function() {
-  shrinkNavbar();
-};
+// window.onscroll = function() {
+//   shrinkNavbar();
+// };
 
-function shrinkNavbar() {
-  var navbar = document.getElementById("topnav-holder");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      navbar.style.padding = "0vh 10vw"; // Set the desired height when scrolled
-  } else {
-      navbar.style.padding = "3vh 10vw"; // Set the original height when at the top
-  }
-};
+// function shrinkNavbar() {
+//   var navbar = document.getElementById("topnav-holder");
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//       navbar.style.padding = "0vh 10vw"; // Set the desired height when scrolled
+//   } else {
+//       navbar.style.padding = "3vh 10vw"; // Set the original height when at the top
+//   }
+// };
