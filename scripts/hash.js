9ba5f5547cc2b6432bff41618c85758c3e129431
mbetz <mbetzel1@gmail.com>
2024-01-28 19:55:32 -0500

    remove topnav when using links

--- scripts/hash.js
+++ scripts/hash.js
@@ -2,6 +2,7 @@ function offsetAnchor() {
     if(location.hash.length !== 0) {
         window.scrollTo(window.scrollX, window.scrollY - 100);
         console.log(location.hash)
+        document.getElementById("#topnav-holder").classList.add("hidden");
     }
 }
 
