732eaaf3d5b039c639ba871dd61e5f6a49f17c17
mbetz <mbetzel1@gmail.com>
2024-01-28 21:48:38 -0500

    fixed jittering on navbar shrink

--- scripts/navbar_hide.js
+++ scripts/navbar_hide.js
@@ -1,28 +1,22 @@
 let prevScrollPos = window.scrollY;
 
-function removePadding() {
-  document.getElementById('topnav-holder').style.padding = "0vh 10vw"
-}
-
-function addPadding() {
-  document.getElementById('topnav-holder').style.padding = "3vh 10vw"
-}
 
 window.onscroll = function() {
   let currentScrollPos = window.scrollY;
   var navbar = document.getElementById("topnav-holder");
   if (prevScrollPos > currentScrollPos) {
     navbar.classList.remove("hidden");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+
+      navbar.style.padding = "0vh 10vw"; // Set the desired height when scrolled
+    } else {
+      navbar.style.padding = "3vh 10vw"; // Set the original height when at the top
+  }
   } else {
     if (currentScrollPos > window.innerHeight / 3) {
       navbar.classList.add("hidden");
     }
   }
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-
-      window.setTimeout(removePadding, 1) // Set the desired height when scrolled
-  } else {
-      window.setTimeout(addPadding, 1) // Set the original height when at the top
-  }
+  
   prevScrollPos = currentScrollPos;
 };
